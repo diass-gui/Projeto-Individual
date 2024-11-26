13b1f0419ad28a21c6117e91dea80544cb9e78f3
diass-gui <enriqueguilherme2@gmail.com>
2024-11-25 21:08:41 -0300

    Atualização e integração da API com o site e o BD

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ var app = express();
 app.use(express.static(path.join(__dirname, "public")));
 
 var usuarioRouter = require("./src/routes/usuarios");
-// var indexRouter = require("./src/routes/index");
+var indexRouter = require("./src/routes/index");
 
 
 
@@ -30,7 +30,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(cors());
 
-// app.use("/", indexRouter);   
+app.use("/", indexRouter);   
 app.use("/usuarios", usuarioRouter);
 
 app.listen(PORTA_APP, function () {
